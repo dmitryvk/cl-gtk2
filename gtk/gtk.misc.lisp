9c9533da2304de4c58f3e917be9ec6cb9f9154bc
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2009-03-19 02:18:19 +0300

    added get-clipboard

--- gtk/gtk.misc.lisp
+++ gtk/gtk.misc.lisp
@@ -2,3 +2,8 @@
 
 (defcallback stable-pointer-free-destroy-notify-callback :void ((data :pointer))
   (free-stable-pointer data))
+
+(defcfun (get-clipboard "gtk_clipboard_get") g-object
+  (selection gdk-atom-as-string))
+
+(export 'get-clipboard)
