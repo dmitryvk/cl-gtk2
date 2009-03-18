82395678dab5ebdfebde620b2d30875d9b33ef75
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2009-03-18 13:20:34 +0300

    moved functions to proper files

--- gtk/gtk.functions.lisp
+++ gtk/gtk.functions.lisp
@@ -1,24 +1,4 @@
 (in-package :gtk)
 
-(defcfun gtk-widget-show-all :void
-  (widget (g-object widget)))
-
-(defcfun gtk-widget-queue-draw :void
-  (widget (g-object widget)))
-
-(defcfun gtk-widget-create-pango-layout (g-object gdk::pango-layout)
-  (widget (g-object widget))
-  (text :string))
-
 (defcfun (object-destroy "gtk_object_destroy") :void
   (object (g-object gtk-object)))
-
-(defcfun gtk-text-buffer-insert :void
-  (buffer (g-object text-buffer))
-  (iter :pointer)
-  (text :string)
-  (len :int))
-
-(defun text-buffer-insert (buffer iter text)
-  (declare (ignore iter))
-  (gtk-text-buffer-insert buffer (null-pointer) text (length text)))
--- gtk/gtk.widget.lisp
+++ gtk/gtk.widget.lisp
@@ -1,3 +1,13 @@
 (in-package :gtk)
 
 ; TODO: GtkWidget
+
+(defcfun gtk-widget-show-all :void
+  (widget (g-object widget)))
+
+(defcfun gtk-widget-queue-draw :void
+  (widget (g-object widget)))
+
+(defcfun gtk-widget-create-pango-layout (g-object gdk::pango-layout)
+  (widget (g-object widget))
+  (text :string))
