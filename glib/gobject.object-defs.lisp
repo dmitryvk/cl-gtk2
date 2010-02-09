7bca05f5d857c03b74eafd7c0c85495dfe3330ff
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2010-02-10 01:10:59 +0300

    Add some type initializers to classes to remove some warnings

--- glib/gobject.object-defs.lisp
+++ glib/gobject.object-defs.lisp
@@ -4,4 +4,5 @@
   ()
   (:metaclass gobject-class)
   (:g-type-name . "GInitiallyUnowned")
+  (:g-type-initializer . "g_initially_unowned_get_type")
   (:documentation "Base class that has initial \"floating\" reference."))
