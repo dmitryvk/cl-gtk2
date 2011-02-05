a2741b58f32035175efccf50bf783d2750dda1c2
Jan Moringen <scymtym@gmx.net>
2011-02-05 18:48:34 +0100

    Fixed a documentation string in cairo/cairo.lisp

--- cairo/cairo.lisp
+++ cairo/cairo.lisp
@@ -4,9 +4,9 @@
 
 (defclass gdk-context (cl-cairo2:context)
   ())
-                          
+
 (defun create-gdk-context (gdk-drawable)
-  "creates an context to draw on a GTK widget, more precisely on the
+  "Creates a context to draw on a GTK widget, more precisely on the
 associated gdk-window.  This should only be called from within the
 expose event.  In cells-gtk, use (gtk-adds-widget-window gtk-pointer)
 to obtain the gdk-window. 'gtk-pointer' is the pointer parameter
